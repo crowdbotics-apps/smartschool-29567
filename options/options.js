b17161e0c9402704279a63ad8d04c5fb92200d55
Crowdbotics <team@crowdbotics.com>
2021-08-06 15:12:46 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "smartschool_29567Identifier",
+  url: "https://smartschool_29567Identifier.botics.co",
+  api: "https://smartschool_29567Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
